5dc2c11c15e9c902720c51cb2988cec7e9e56616
Harold Kurth <harold.kurth@gmail.com>
2015-07-28 13:30:19 -0500

    Initial Commit

---
+++ New-P-O-C/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  New-P-O-C
+//
+//  Created by Harold Kurth on 7/28/15.
+//  Copyright © 2015 Harold Kurth. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
